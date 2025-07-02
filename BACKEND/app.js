b7d38970fb84a0401714e9be0dc4ddf0d8b19a7f
Jayant9917 <ranajayant527@gmail.com>
2025-07-03 00:25:31 +0530

    Mongodb connection

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,8 +1,14 @@
 import express from 'express';
-
+import { nanoid } from 'nanoid';
+import connectDB from './src/config/mongo.config.js';
 const app = express();
 const PORT = 3000;
-import { nanoid } from 'nanoid';
+
+import dotenv from 'dotenv';
+dotenv.config();
+console.log("MONGODB_URI =", process.env.MONGODB_URI);
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
@@ -14,6 +20,7 @@ app.post('/api/create', (req,res) => {
 })
 
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server is running on ${PORT}`);
 })
 
