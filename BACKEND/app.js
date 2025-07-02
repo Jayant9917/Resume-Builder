a0e4fe6f6d7b0304d52a36a1381480c7c88b3cbb
Jayant9917 <ranajayant527@gmail.com>
2025-07-02 23:32:46 +0530

    dummy api

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,7 +1,17 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 const PORT = 3000;
+import { nanoid } from 'nanoid';
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.get('/api/create', (req,res) => {
+    const{ url } = req.body;
+    console.log(url);
+    res.send(nanoid(7));
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
