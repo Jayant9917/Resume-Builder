9e2f8f92db82bed7a12e789e7ec9984203fb732e
Jayant9917 <ranajayant527@gmail.com>
2025-07-07 22:03:55 +0530

    navbar complete

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
-import { landingPageStyles } from '../assets/dummystyle'
+import { LayoutTemplate, Menu, X } from 'lucide-react';
+import { landingPageStyles, } from '../assets/dummystyle'
+import { useState } from 'react'
+
+
 
 const LandingPage = () => {
+
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
   return (
     <div className={landingPageStyles.container}>
       {/* HEADER */}
@@ -9,8 +16,24 @@ const LandingPage = () => {
         <div className={landingPageStyles.headerContainer}>
             <div className={landingPageStyles.logoContainer}>
                 <div className={landingPageStyles.logoIcon}>
-                  
+                  <LayoutTemplate className={landingPageStyles.logoIconInner}/>
                 </div>
+                <span className={landingPageStyles.logoText}>
+                  ResumeXpert
+                </span>
+            </div>
+
+            {/* MOBILE MENU BUTTON */}
+            <button className={landingPageStyles.mobileMenuButton}
+            onClick={() => setMobileMenuOpen(!mobileMenuOpen)}>
+              {mobileMenuOpen ? 
+              <X size={24} className={landingPageStyles.mobileMenuIcon}/> :
+              <Menu size={24} className={landingPageStyles.mobileMenuIcon}/>}
+            </button>
+
+            {/* DESKTOP NAVIGATION */}
+            <div className='hidden md:flex items-center '>
+              {user}
             </div>
         </div>
       </header>
