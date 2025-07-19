af901ac07dc586b8df907b1e8fe68b4da03d0794
Jayant9917 <ranajayant527@gmail.com>
2025-07-19 16:56:38 +0530

    Templates Adding

---
+++ frontend/src/components/Tabs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tabs = ({tabs, activeTab, setActiveTab}) => {
+  return (
+    <div className='w-full my-2'>
+      <div className='flex flex-wrap bg-violet-50 pt-1 rounded-2xl border border-violet-100'></div>
+    </div>
+  )
+}
+
+export default Tabs
